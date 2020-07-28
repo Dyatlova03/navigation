a7eb55a3caabf98b62c3cfcda18bfc12e7f41c0c
Olya Dyatlova <kuzhal97@gmail.com>
2020-07-28 16:39:52 +0300

    head.php

--- blocks/head.php
+++ blocks/head.php
@@ -3,4 +3,5 @@
 ?>
 <meta charset="UTF-8">
     <title><?=$title?></title>
+    <link rel="SHORTCUT ICON" href="img/favicon.ico" type="image/x-icon">
     <link href="/css/style.css" rel="stylesheet" type="text/css" >
--- functions/functions.php
+++ functions/functions.php
@@ -4,26 +4,28 @@
     function connectDB(){
         global $mysqli;
         $mysqli = new mysqli("localhost", "root", "", "navigatorbase");
-        $mysqli->query("SET NAMES 'utf-8'");
+        $mysqli->query("SET NAMES 'utf8mb4'");
     }
 
     function closeDB(){
         global $mysqli;
-        $mysqli->close ();
+        $mysqli->close();
     }
 
-    function getBlocks($id){
+    function getBlocks($limit){
         global $mysqli;
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         connectDB();
-        $result = $mysqli->query("SELECT * FROM 'block' WHERE 'id' = $id");
+        $result = $mysqli->query("SELECT * FROM `block` LIMIT $limit");
         closeDB();
         return resultToArray ($result);
     }
     
   function resultToArray ($result){
         $array = array();
-        while (($row = $result->fetch_assoc()) != false)
+        while (($row = $result->fetch_assoc()) != false){
             $array[] = $row;
+        }
         return $array;
     }
 ?>
